a7bfcb98f336cab47046f08bee63e4ac2a433007
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-05-27 10:16:12 +0100

    make build

---
+++ index.116c412e.js
@@ -0,0 +1,2 @@
+!function e(n,t){if(Object.keys(t).length){var r=document.createElement("ul");for(var a in n.append(r),t){var c=document.createElement("li");c.textContent=a,r.append(c),e(c,t[a])}}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.116c412e.js.map
